29e88b5676aa3f9af1718b75c66a572f3d7c7fe7
Kyle Adams <kyleadams@saras-iPhone-7.MissouriState.edu>
2017-11-01 14:48:17 -0500

    yuo

--- myScript.js
+++ myScript.js
@@ -1,11 +1,11 @@
 
-function validateForm(){
-    var x = document.forms["myForm"]["fname"].value;
-    if (x ==""){
-        alert("Name must be filled out");
-        return false;
-    }
-}
+//function validateForm(){
+//   var x = document.forms["myForm"]["fname"].value;
+ //   if (x ==""){
+  //      alert("Name must be filled out");
+  //      return false;
+  //  }
+//}
 var date = new Date ();
 var n = date.toDateString();
 var time = date.toLocaleTimeString();
