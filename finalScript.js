f8f0531d0db2660dc9cff5490205d093ac4e4623
Kyle Adams <ka1996@live.missouristate.edu>
2017-12-03 17:11:01 -0600

    sdf

--- finalScript.js
+++ finalScript.js
@@ -1,6 +1,8 @@
 function changeLight() {
     var x = document.getElementById("trafficRed");
     var y = document.getElementById("trafficGreen");
+    var distance1 = 0;
+    var distance2 = 0;
     if (x.style.visibility === "visible") 
     {
         x.style.visibility = "hidden";
@@ -14,7 +16,21 @@ function changeLight() {
         x.style.visibility = "visible";
         y.style.visibility = "hidden";
     }
-    
-    var distance = Math.floor((Math.random() * 10) + 1) + 20;
-    document.getElementById("car1").style.left = distance + "px";
+    do 
+    {
+        var distanceCar1 = Math.floor((Math.random() * 10) + 1) + 20;
+        document.getElementById("car1").style.left = distanceCar1 + "px";
+    }
+    while (car1.style.left < 1000)
+    do 
+    {
+        var distanceCar2 = Math.floor((Math.random() * 10) + 1) + 20;
+        document.getElementById("car1").style.left = distanceCar2 + "px";
+    }
+    while (car2.style.left <1000) 
+    {
+        alert("The race is over!");
+    }
+
 }
+
