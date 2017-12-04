b2d07b0cfa00c398a5135b98b11abb323027cff1
Kyle Adams <ka1996@live.missouristate.edu>
2017-12-04 00:17:47 -0600

    yup

--- finalScript.js
+++ finalScript.js
@@ -1,36 +1,40 @@
-function changeLight() {
+function changeLight(){
     var x = document.getElementById("trafficRed");
     var y = document.getElementById("trafficGreen");
+    var car1 = document.getElementById("car1");
+    var car2 = document.getElementById("car2");
     var distance1 = 0;
     var distance2 = 0;
-    if (x.style.visibility === "visible") 
-    {
-        x.style.visibility = "hidden";
-    } 
-    if (y.style.visibility === "hidden")
-    {
-        y.style.visibility = "visible";
-    }
-    else 
-    {
-        x.style.visibility = "visible";
-        y.style.visibility = "hidden";
-    }
-    do 
-    {
-        var distanceCar1 = Math.floor((Math.random() * 10) + 1) + 20;
-        document.getElementById("car1").style.left = distanceCar1 + "px";
-    }
-    while (car1.style.left < 1000)
-    do 
-    {
-        var distanceCar2 = Math.floor((Math.random() * 10) + 1) + 20;
-        document.getElementById("car1").style.left = distanceCar2 + "px";
-    }
-    while (car2.style.left <1000) 
-    {
-        alert("The race is over!");
-    }
+    var total1 = 20;
+    var total2 = 20;
 
+    y.style.visibility = 'visible';
+    x.style.visibility = 'hidden';
+
+    var pace1 = setInterval(frame, 10);
+    var pace2 = setInterval(frame, 10);
+    function frame() {
+        console.log(total1);
+      if (total1 >= 1000) {
+        clearInterval (pace1);
+        clearInterval (pace2);
+        alert("Congratulations top car won!");
+      } else {
+        distance1 = Math.floor((Math.random() * 10) + 1 );
+        total1 += distance1;
+        car1.style.left = total1 + 'px'; 
+      }
+      console.log(total2);
+      if (total2 >= 1000){
+        clearInterval (pace1);
+        clearInterval (pace2);
+        alert("Congratulations bottom car won!");
+      } 
+      else{
+      distance2 = Math.floor((Math.random() * 10) + 1 );
+      total2 += distance2;
+      car2.style.left = total2 + 'px'; 
+      }
+    }
 }
 
